c3077411e9d8f2b142366707e0ec03abbee0fbf2
Brent Williams <becauseinterwebs@gmail.com>
2025-04-04 12:15:15 -0500

    update Firebase connection

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+/**
+ * NOTE: Uncomment FirebaseProvider entries once you have configured your Firebase connection
+ */
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+//import FirebaseProvider from '@/providers/FirebaseProvider';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/*<FirebaseProvider>*/}
+      <App />
+    {/*</FirebaseProvider>*/}
   </StrictMode>,
 )
